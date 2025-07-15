13daf07411d6e4a9a40dcb69be59401afd9a8e7b
AdelZinnurova <ad187174@bk.ru>
2025-07-15 10:02:23 +0300

    add css Home page

--- src/widgets/main/Home/Home.tsx
+++ src/widgets/main/Home/Home.tsx
@@ -1,22 +1,23 @@
-import styles from './Home.module.css';
+import s from './Home.module.css';
 import {Button} from "@/shared/ui/Button/Button.tsx";
 
 export const Home = () => {
     return (
-        <section className={styles.section}>
-            <div className={styles.leftColumn}>
-                <h1 className={styles.mainHeadline}>Transform your vision of services</h1>
-                <p className={styles.text}>MyServe is the first three-sided digital marketplace for the service industry that seamlessly connects
-                    equipped workspace owners, freelancers, and customers</p>
+        <section className={s.section}>
+            <div className={s.leftColumn}>
+                <h1 className={s.mainHeadline}>Transform your vision of services</h1>
+                <p className={`${s.textDescription} textSecondary`}> MyServe is the first three-sided digital marketplace
+                    for the service industry that seamlessly connects equipped
+                    workspace owners, freelancers and customers</p>
                 <Button title={"Explore"}/>
             </div>
-            <div>
-                <iframe
-                    src={"https://youtu.be/d0hXWo5fPdw?si=AFrqY7MoyqWDFOdj"}
-                    title="MyServe Video"
-                    frameBorder="0"
-                ></iframe>
-            </div>
+            <iframe
+                className={s.video}
+                src="https://www.youtube.com/embed/d0hXWo5fPdw?si=xFkjbUbGnosW3qwO"
+                title="MyServe"
+                frameBorder="0"
+                allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen={true}/>
         </section>
     );
 };
