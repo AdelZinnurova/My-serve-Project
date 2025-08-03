5673d7a7e8424a5299abc7a8bc10858264146ef0
AdelZinnurova <ad187174@bk.ru>
2025-08-03 13:21:57 +0300

    correct Users

--- src/widgets/main/Users/Users.tsx
+++ src/widgets/main/Users/Users.tsx
@@ -1,16 +1,21 @@
 import {usersCard} from "@/data/usersCard.ts";
 import {UsersCard} from "@/widgets/main/Users/UsersCard/UsersCard.tsx";
+import {Container} from "@/app/layout/Container";
 import s from './Users.module.css';
 
 export const Users = () => {
     return (
         <section id="section3" className={s.usersSection}>
-            <h2 className={s.usersTitle}>Why MyServe is the perfect fit for you?</h2>
-            <div className={s.usersCardsWrap}>
-                {usersCard.map((card) => (
-                    <UsersCard key={card.id} {...card}/>
-                ))}
-            </div>
+            <Container>
+                <div className={s.sectionWrap}>
+                    <h2 className={s.usersTitle}>Why MyServe is the perfect fit for you?</h2>
+                    <div className={s.usersCardsWrap}>
+                        {usersCard.map((card) => (
+                            <UsersCard key={card.id} {...card}/>
+                        ))}
+                    </div>
+                </div>
+            </Container>
         </section>
     )
 }
