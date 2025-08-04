22938e357b458def2fd07d9427d0c611fadef88e
AdelZinnurova <ad187174@bk.ru>
2025-08-04 13:59:40 +0300

    correct Product

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
 import './App.css'
-import s from './App.module.css';
 import {Header} from "@/widgets/header/Header.tsx";
 import {Main} from "@/widgets/main/Main.tsx";
 import {Footer} from "@/widgets/footer/Footer.tsx";
 
 function App() {
     return (
-        <div className={s.app}>
+        <>
             <Header/>
             <Main/>
             <Footer/>
-        </div>
+        </>
     )
 }
 
